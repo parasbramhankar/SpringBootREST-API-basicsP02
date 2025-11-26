433da9f7b5ac2e9a57448911804843d4d1fb2aa2
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-26 10:16:30 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootREST_API_basicsP02/SpringBootRestApiBasicsP02Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootREST_API_basicsP02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiBasicsP02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiBasicsP02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootREST_API_basicsP02/SpringBootRestApiBasicsP02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootREST_API_basicsP02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiBasicsP02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
